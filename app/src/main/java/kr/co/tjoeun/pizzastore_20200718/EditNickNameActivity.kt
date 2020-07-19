6b83cb87edb094ca29d317e242978a011a821c23
yd27 <dhdbseh5oe@naver.com>
2020-07-19 10:27:13 +0900

    입력한 닉네임을 가지고 복귀

--- app/src/main/java/kr/co/tjoeun/pizzastore_20200718/EditNickNameActivity.kt
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200718/EditNickNameActivity.kt
@@ -1,10 +1,24 @@
 package kr.co.tjoeun.pizzastore_20200718
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_nick_name.*
 
 class EditNickNameActivity : BaseActivity() {
     override fun setupEvents() {
+
+        okBtn.setOnClickListener {
+            //입력한 닉네임을 저장
+            val inputNickName = newNickNameEdt.text.toString()
+
+            //Intent에 담아서 복귀
+            val resultIntent = Intent()
+            resultIntent.putExtra("nickName", inputNickName)
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+        }
 
     }
 
