205f2af6078e86570d1cde083cbc6b5663ec847e
yd27 <dhdbseh5oe@naver.com>
2020-07-19 10:13:54 +0900

    내 프로필 프래그먼트 변경

---
+++ app/src/main/java/kr/co/tjoeun/pizzastore_20200718/ViewPizzaStoreDetailActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.tjoeun.pizzastore_20200718
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPizzaStoreDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_pizza_store_detail)
+    }
+}
